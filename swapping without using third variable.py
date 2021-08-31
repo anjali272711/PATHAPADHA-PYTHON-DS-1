d448334185d36aef67fa2535f5b145351424696e
anjali272711 <89823387+anjali272711@users.noreply.github.com>
2021-08-31 12:14:26 +0530

    Add files via upload

---
+++ swapping without using third variable.py
@@ -0,0 +1,5 @@
+a = int(input("Enter number a: "))
+b = int(input("Enter number b: "))
+print("Before swapping the number a: ",a,"b: ",b)
+a,b = b,a
+print("After swapping the number a: ",a,"b: ",b)
