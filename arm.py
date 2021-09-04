e78469a15f26bb75d93b5399b0453e5934f23a58
anjali272711 <89823387+anjali272711@users.noreply.github.com>
2021-09-04 21:53:12 +0530

    Add files via upload

--- arm.py
+++ arm.py
@@ -12,3 +12,18 @@ if(sum == num):
   print(num,"is an armstrong number")
 else:
   print(num,"is not an armstrong number")
+
+
+# WAP to print the sum of digits of a number
+
+
+
+
+num = int(input("Enter a number "))
+sum = 0
+temp = num
+while temp>0:
+  digit = temp%10
+  sum+= digit
+  temp//= 10
+print("sum of digits of",num,"is :",sum)
